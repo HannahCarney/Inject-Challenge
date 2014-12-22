61a7433786c614a85df0ef6f7f78d2eb275028ac
Hannah Carney <hannahcarneyart@gmail.com>
2014-12-22 13:45:46 +0000

    Inject can add, minus and multiply using blocks

---
+++ spec/inject_spec.rb
@@ -0,0 +1,20 @@
+require 'inject'
+
+describe Array do
+
+  context "with a block" do
+
+    it "can add numbers together" do
+      expect([1,2,3,4].inject{|memo, item| memo + item}).to eq 10
+      expect([1,2,3,4].my_inject{|memo, item| memo + item}).to eq 10
+    end
+
+    it "can minus numbers" do
+      expect([1,2,3,4].inject{|memo, item| memo - item}).to eq -8
+    end
+
+    it "can multiply numbers" do
+      expect([1,2,3,4].inject{|memo, item| memo * item}).to eq 24
+    end
+  end
+end
