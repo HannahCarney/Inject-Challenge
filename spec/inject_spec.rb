79ff15d829d34154766922eefd88c46817715dee
Hannah Carney <hannahcarneyart@gmail.com>
2014-12-22 14:03:56 +0000

    added tests for inject with argument and block

--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -30,4 +30,18 @@ describe Array do
       expect([1,2,3,4].inject(:*)).to eq 24
     end
   end
+
+  context "with a argument and block" do
+    it "can add numbers together" do
+      expect([1,2,3,4].inject(10){|memo, item| memo + item}).to eq 20
+    end
+
+    it "can minus numbers" do
+      expect([1,2,3,4].inject(10){|memo, item| memo - item}). to eq 0
+    end
+
+    it "can multiply numbers" do
+      expect([1,2,3,4].inject(10){|memo, item| memo * item}). to eq 240
+    end
+  end  
 end
