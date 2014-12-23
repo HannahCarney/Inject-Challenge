65b078538de674b3d04a9b6a270d8089c652b5a8
Hannah Carney <hannahcarneyart@gmail.com>
2014-12-23 15:38:33 +0000

    added method for addition to inject

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,7 +1,21 @@
 class Array
 
-  def my_inject
-    10 || -8
-  end
-
+ def my_inject(*args)
+    if block_given?
+      copy = self.dup
+      memo = copy.shift
+      item = copy[0]
+      copy.each do |item|
+      memo = yield memo, item
+    end
+      end
+      memo 
+    end
+    
 end
+
+
+# case number
+
+# when add
+# when minus
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -8,132 +8,134 @@ describe Array do
       expect([1,2,3,4].my_inject{|memo, item| memo + item}).to eq 10
     end
 
-    it "can minus numbers" do
-      expect([1,2,3,4].inject{|memo, item| memo - item}).to eq -8
-      # expect([1,2,3,4].my_inject{|memo, item| memo - item}). to eq -8
-    end
 
-    it "can multiply numbers" do
-      expect([1,2,3,4].inject{|memo, item| memo * item}).to eq 24
-    end
 
-    it "can divide numbers and rounds them to integer" do
-      expect([1,2,3,4].inject{|memo, item| memo / item}).to eq 0
-    end
+  #   it "can minus numbers" do
+  #     expect([1,2,3,4].inject{|memo, item| memo - item}).to eq -8
+  #     expect([1,2,3,4].my_inject{|memo, item| memo - item}). to eq -8
+  #   end
 
-    it "can find the remainder after divided" do
-      expect([1,2,3,4].inject{|memo, item| memo % item}).to eq 1
-    end
+  #   it "can multiply numbers" do
+  #     expect([1,2,3,4].inject{|memo, item| memo * item}).to eq 24
+  #   end
 
-    it "can prefrom exponential calculation" do
-      expect([1,2,3,4].inject{|memo, item| memo ** item}).to eq 1
-    end
-  end
+  #   it "can divide numbers and rounds them to integer" do
+  #     expect([1,2,3,4].inject{|memo, item| memo / item}).to eq 0
+  #   end
 
-  context "with a symbol" do 
-    it "can add numbers together" do
-      expect([1,2,3,4].inject(:+)). to eq 10
-    end
-    it "can minus numners" do
-      expect([1,2,3,4].inject(:-)).to eq -8
-    end
+  #   it "can find the remainder after divided" do
+  #     expect([1,2,3,4].inject{|memo, item| memo % item}).to eq 1
+  #   end
 
-    it "can multiply numbers" do
-      expect([1,2,3,4].inject(:*)).to eq 24
-    end
+  #   it "can prefrom exponential calculation" do
+  #     expect([1,2,3,4].inject{|memo, item| memo ** item}).to eq 1
+  #   end
+  # end
 
-    it "can divide numbers" do
-      expect([1,2,3,4].inject(:/)).to eq 0
-    end
+  # context "with a symbol" do 
+  #   it "can add numbers together" do
+  #     expect([1,2,3,4].inject(:+)). to eq 10
+  #   end
+  #   it "can minus numners" do
+  #     expect([1,2,3,4].inject(:-)).to eq -8
+  #   end
 
-    it "can find the remainder after divided" do
-      expect([1,2,3,4].inject(:%)).to eq 1
-    end
+  #   it "can multiply numbers" do
+  #     expect([1,2,3,4].inject(:*)).to eq 24
+  #   end
 
-    it "can prefrom exponential calculation" do
-      expect([1,2,3,4].inject(:**)).to eq 1
-    end
-  end
+  #   it "can divide numbers" do
+  #     expect([1,2,3,4].inject(:/)).to eq 0
+  #   end
 
-  context "with a argument and block" do
-    it "can add numbers together with a starting point" do
-      expect([1,2,3,4].inject(10){|memo, item| memo + item}).to eq 20
-    end
+  #   it "can find the remainder after divided" do
+  #     expect([1,2,3,4].inject(:%)).to eq 1
+  #   end
 
-    it "can minus numbers with a starting point" do
-      expect([1,2,3,4].inject(10){|memo, item| memo - item}).to eq 0
-    end
+  #   it "can prefrom exponential calculation" do
+  #     expect([1,2,3,4].inject(:**)).to eq 1
+  #   end
+  # end
 
-    it "can multiply numbers with a starting point" do
-      expect([1,2,3,4].inject(10){|memo, item| memo * item}).to eq 240
-    end
+  # context "with a argument and block" do
+  #   it "can add numbers together with a starting point" do
+  #     expect([1,2,3,4].inject(10){|memo, item| memo + item}).to eq 20
+  #   end
 
-    it "can divide numbers with a starting point" do
-      expect([1,2,3,4].inject(10){|memo, item| memo / item}).to eq 0
-    end
+  #   it "can minus numbers with a starting point" do
+  #     expect([1,2,3,4].inject(10){|memo, item| memo - item}).to eq 0
+  #   end
 
-    it "can find the remainder after divided with a starting point" do
-      expect([1,2,3,4].inject(10){|memo, item| memo % item}).to eq 0
-    end
+  #   it "can multiply numbers with a starting point" do
+  #     expect([1,2,3,4].inject(10){|memo, item| memo * item}).to eq 240
+  #   end
 
-    it "can prefrom exponential calculation with a starting point" do
-      expect([2,3].inject(2){|memo, item| memo ** item}).to eq 64
-    end
-  end
+  #   it "can divide numbers with a starting point" do
+  #     expect([1,2,3,4].inject(10){|memo, item| memo / item}).to eq 0
+  #   end
+
+  #   it "can find the remainder after divided with a starting point" do
+  #     expect([1,2,3,4].inject(10){|memo, item| memo % item}).to eq 0
+  #   end
+
+  #   it "can prefrom exponential calculation with a starting point" do
+  #     expect([2,3].inject(2){|memo, item| memo ** item}).to eq 64
+  #   end
+  # end
   
-  context "with a symbol and an argument" do
-    it "can add with a starting point and a symbol" do
-      expect([1,2,3,4].inject(10,:+)).to eq 20
-    end
+  # context "with a symbol and an argument" do
+  #   it "can add with a starting point and a symbol" do
+  #     expect([1,2,3,4].inject(10,:+)).to eq 20
+  #   end
 
-    it "can minus with a starting point and a symbol" do
-      expect([1,2,3,4].inject(10, :-)).to eq 0
-    end
+  #   it "can minus with a starting point and a symbol" do
+  #     expect([1,2,3,4].inject(10, :-)).to eq 0
+  #   end
     
-    it "can multiply with a starting point and a symbol" do
-      expect([1,2,3,4].inject(10, :*)).to eq 240
-    end
+  #   it "can multiply with a starting point and a symbol" do
+  #     expect([1,2,3,4].inject(10, :*)).to eq 240
+  #   end
 
-    it "can divide numbers with a starting point and a symbol" do
-      expect([1,2,3,4].inject(10, :/)).to eq 0
-    end
+  #   it "can divide numbers with a starting point and a symbol" do
+  #     expect([1,2,3,4].inject(10, :/)).to eq 0
+  #   end
 
-    it "can find the remainder after divided" do
-      expect([1,2,3,4].inject(10, :%)).to eq 0
-    end
+  #   it "can find the remainder after divided" do
+  #     expect([1,2,3,4].inject(10, :%)).to eq 0
+  #   end
 
-    it "can prefrom exponential calculation with symbol and argument" do
-      expect([2,3].inject(2, :**)).to eq 64
-    end   
-  end
+  #   it "can prefrom exponential calculation with symbol and argument" do
+  #     expect([2,3].inject(2, :**)).to eq 64
+  #   end   
+  # end
 
-  context "can compare numbers to each other" do
-    it "can check to see if one number is greater than another" do
-      expect([1,2].inject(:<)).to be true
-    end
+  # context "can compare numbers to each other" do
+  #   it "can check to see if one number is greater than another" do
+  #     expect([1,2].inject(:<)).to be true
+  #   end
 
-    it "can check is less than or equal to" do
-      expect([2,2].inject(:<=)).to be true
-    end
+  #   it "can check is less than or equal to" do
+  #     expect([2,2].inject(:<=)).to be true
+  #   end
 
     it "can check to see if two numbers are the same" do
       expect([1,1].inject(:==)).to be true
     end
 
-    it "can check to see if two numbers are not the same" do
-      expect([1,2].inject(:!=)).to be true
-    end
+#     it "can check to see if two numbers are not the same" do
+#       expect([1,2].inject(:!=)).to be true
+#     end
 
-    it "can return -1, 0, 1 depending on if numbers are same or different" do
-      expect([1,2].inject(:<=>)). to eq -1
-    end
-  end
+#     it "can return -1, 0, 1 depending on if numbers are same or different" do
+#       expect([1,2].inject(:<=>)). to eq -1
+#     end
+#   end
 
-    context "can work with words as well as numnbers" do
+#     context "can work with words as well as numnbers" do
 
-    it "can find the longest word" do
-      expect([:cat, :hat, :mouse].inject{|memo, word| memo.length > word.length ? memo : word}).to eq(:mouse)
-    end
+#     it "can find the longest word" do
+#       expect([:cat, :hat, :mouse].inject{|memo, word| memo.length > word.length ? memo : word}).to eq(:mouse)
+#     end
 
 
   end
